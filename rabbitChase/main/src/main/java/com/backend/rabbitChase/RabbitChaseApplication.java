b698d1c2af80f14f53c923f904527cb6c9fc5311
Vadim Uniatiskiy <rich246135@gmail.com>
2025-01-06 22:03:38 +0300

    Basic

---
+++ rabbitChase/main/src/main/java/com/backend/rabbitChase/RabbitChaseApplication.java
@@ -0,0 +1,15 @@
+package com.backend.rabbitChase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RabbitChaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitChaseApplication.class, args);
+	}
+
+}
---
+++ rabbitChase/main/src/test/java/com/backend/rabbitChase/RabbitChaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.rabbitChase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitChaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
